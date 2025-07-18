ded92295ed4f5374de64b8e2364cead3c4c6deb5
Baddi <cvremo69@gmail.com>
2025-07-18 15:14:08 +0530

    pushing again

---
+++ src/main/java/com/example/newjars/New.java
@@ -0,0 +1,21 @@
+package com.example.newjars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class New {
+    public static void main(String[] args) {
+
+        SpringApplication.run(NewjarsApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String indexs() {
+        return "indexs"; // maps to src/main/resources/templates/index.html
+    }
+
+}
---
+++ src/main/java/com/example/newjars/NewjarsApplication.java
@@ -0,0 +1,13 @@
+package com.example.newjars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewjarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewjarsApplication.class, args);
+	}
+
+}
